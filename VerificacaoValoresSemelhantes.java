e43da989ff49dd0e212525b66a2b32f158d05ca2
Pedro Henrique Neves <pedrinhohneves14@gmail.com>
2025-05-26 15:22:02 -0300

    VerificacaoValoresSemelhantes.java

---
+++ VerificacaoValoresSemelhantes.java
@@ -0,0 +1,11 @@
+// Repetição de transações com valores semelhantes para contas recém-criadas.
+
+import java.util.ArrayList;
+
+public class VerificacaoValoresSemelhantes {
+    
+    // fazer uma verificador da criação da conta
+    //fazer uma cerificação dos valores das transferencias
+
+
+}
