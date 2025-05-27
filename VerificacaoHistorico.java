6ae8b2a5e07e78c06927fec18a2d742fd5557e2d
Pedro Henrique Neves <pedrinhohneves14@gmail.com>
2025-05-27 13:36:45 -0300

    VerificacaoHistorico.java

--- VerificacaoHistorico.java
+++ VerificacaoHistorico.java
@@ -5,7 +5,7 @@ public class VerificacaoHistorico {
     private final Timer timer = new Timer();
     private final ArrayList<String> historicoMovimentacoes = new ArrayList<>();
     private final Date dataCriacaoConta;
-    private boolean acusarHistorico;
+
 
     private static final long TempoContaRecente = 180L * 24 * 60 * 60 * 1000;
 
@@ -29,10 +29,8 @@ public class VerificacaoHistorico {
         long idadeConta = System.currentTimeMillis() - dataCriacaoConta.getTime();
 
         if (idadeConta <= TempoContaRecente && historicoMovimentacoes.size() <= 10) {
-            acusarHistorico = true;
             System.out.println("Conta recente e com pouco histórico de movimentação.");
         } else {
-            acusarHistorico = false;
             System.out.println("Conta com movimentação aceitável ou já tem mais de 180 dias.");
         }
     }
